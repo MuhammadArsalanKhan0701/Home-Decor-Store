2bd09b36a74e06f6410faa98798485679cb643ed
MuhammadArsalanKhan0701 <87452814+MuhammadArsalanKhan0701@users.noreply.github.com>
2023-03-21 20:24:26 +0500

    added footer

--- src/components/FooterBottom/FooterBottom.js
+++ src/components/FooterBottom/FooterBottom.js
@@ -1,6 +1,34 @@
 import styles from './FooterBottom.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons';
 
 const FooterBottom = () => {
+
+  return (
+    <div className={styles.footerBottom}>
+      <p className={styles.footerBottomP}>
+        © 2021 Qode Interactive, All Rights Reserved
+      </p>
+      <div>
+        <span className={styles.footerBottomSpan}>Follow Us</span>
+        <a href="https://www.twitter.com">
+          <FontAwesomeIcon
+            icon={faTwitter}
+            className={styles.footerBottomTwitter} />
+        </a>
+        <a href="https://www.instagram.com">
+          <FontAwesomeIcon
+            icon={faInstagram}
+            className={styles.footerBottomInstagram} />
+        </a>
+        <a href="https://www.facebook.com">
+          <FontAwesomeIcon
+            icon={faFacebookF}
+            className={styles.footerBottomFacebook} />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default FooterBottom;
